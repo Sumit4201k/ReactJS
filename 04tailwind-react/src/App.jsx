fda1f949fcab6870b122daab8e785f99cae723a4
Sumit4201k <kamtisumit6856@gmail.com>
2025-12-13 18:23:59 +0530

    learned tailwind css importing in react and installation with a sample tamplate

---
+++ 04tailwind-react/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-fuchsia-400 text-black rounded-2xl padding-10px'>tailwind css</h1>
+
+
+
+    
+
+
+
+
+    </>
+  )
+}
+
+
+
+export default App
---
+++ 04tailwind-react/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Example from './tailwind.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+    <Example />
+  </React.StrictMode>
+)
