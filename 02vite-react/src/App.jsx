1036068d23b2fc5173e291e2331cdb2bb3b343cc
Sumit4201k <kamtisumit6856@gmail.com>
2025-12-13 11:36:04 +0530

    learning diffrence between react and vite react

---
+++ 02vite-react/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  
+
+  return (
+  <div>
+    
+    <h1>
+      vite react 
+      </h1>
+      <p> thr diff between react and vite react is componenet 
+        naming rules and some pre installed dependencies </p>
+  </div>
+  )
+}
+
+export default App
